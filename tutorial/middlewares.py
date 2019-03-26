bfe4592a5089ebd31b23bff24b8b9da7d5f6bf63
Paulo Moggi <paulohpmoggi@gmail.com>
2019-03-26 16:02:35 -0300

    Random user agent

--- tutorial/middlewares.py
+++ tutorial/middlewares.py
@@ -4,8 +4,23 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+import random
 from scrapy import signals
+
+
+# https://raw.githubusercontent.com/scrapinghub/testspiders/master/testspiders/middleware.py
+class RandomUserAgent(object):
+    """Randomly rotate user agents based on a list of predefined ones"""
+
+    def __init__(self, agents):
+        self.agents = agents
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+
+    def process_request(self, request, spider):
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
 
 
 class TutorialSpiderMiddleware(object):
